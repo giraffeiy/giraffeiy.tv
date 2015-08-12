a3bab56edf11aec8c818c817a708322a52419231
giraffwing <120152105@qq.com>
2015-08-12 09:44:00 +0800

    安装和更新自定义主题

---
+++ sites/all/themes/giraffeiy_admin/template.php
@@ -0,0 +1,13 @@
+<?php
+
+function giraffeiy_admin_preprocess_html(&$variables){
+  $theme_path = drupal_get_path('theme','giraffeiy_admin');
+  drupal_add_css($theme_path.'/css/style.css',
+  [
+    'group' =>  CSS_THEME,
+    'media' =>  'all',
+    'weight'  =>  1000,
+    ]);
+}
+
+ ?>
