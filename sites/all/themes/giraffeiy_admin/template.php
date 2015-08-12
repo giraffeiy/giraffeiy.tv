1b21a6970a8e658ebaa1f0f7a66e3f95d99cba9f
giraffwing <120152105@qq.com>
2015-08-12 10:46:57 +0800

    giraffe_admin模板修改

--- sites/all/themes/giraffeiy_admin/template.php
+++ sites/all/themes/giraffeiy_admin/template.php
@@ -1,13 +1,10 @@
 <?php
-
-function giraffeiy_admin_preprocess_html(&$variables){
-  $theme_path = drupal_get_path('theme','giraffeiy_admin');
-  drupal_add_css($theme_path.'/css/style.css',
-  [
-    'group' =>  CSS_THEME,
-    'media' =>  'all',
-    'weight'  =>  1000,
+function giraffeiy_admin_preprocess_html(&$variables) {
+  $theme_path = drupal_get_path('theme', 'giraffeiy_admin');
+  drupal_add_css($theme_path . '/css/style.css',
+    [
+      'group'     => CSS_THEME,
+      'media'     => 'all',
+      'weight'    => 1000
     ]);
 }
-
- ?>
